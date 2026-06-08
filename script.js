41b2fc114c8db75d1dec2bff8f96d354432e4a2f
M7hsin <muhdmuhsinepic377@gmail.com>
2026-06-08 10:49:13 +0800

    Latest

---
+++ script.js
@@ -0,0 +1,10 @@
+$(function() {
+
+  // Your jQuery code goes here
+  // This runs after the page fully loads
+
+  $('#my-button').on('click', function() {
+    $('#result').text('Button clicked!');
+  });
+
+});
